1b75685403e8739b1ea23451f021e8a178c9cbc0
fabricioreisf <fabricioreisferreira@hotmail.com>
2026-04-06 10:59:53 -0300

    refactor: ordering code

--- contracts/Candidate.sol
+++ contracts/Candidate.sol
@@ -4,28 +4,28 @@ pragma solidity 0.8.28;
 
 contract Candidate {
     
-    address public owner;
-    uint256 public productCount;
-    uint256 public totalRevenue;
-    mapping(uint256 => Product) public products;
-    mapping(address => uint256) public purchases; 
-
-    modifier onlyOwner() {
-        require(owner == msg.sender, "Only owner can call this function");
-        _;
-    }
-
-    constructor() {
-        owner = msg.sender;
-    }
-
     struct Product {
         uint256 id;
         string name;
         uint256 price;
         uint256 quantity;
         bool exists;
-    }   
+    }
+    
+    address public owner;
+    uint256 public productCount;
+    uint256 public totalRevenue;
+    mapping(uint256 => Product) public products;
+    mapping(address => uint256) public purchases;
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "Only owner can call this function");
+        _;
+    } 
+
+    constructor() {
+        owner = msg.sender;
+    }
 
     // Product management
     function registrarProduto(string memory _nome, uint256 _preco, uint256 _quantidade) public onlyOwner returns (uint256) {}

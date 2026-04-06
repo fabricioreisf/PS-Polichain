92e99595009cd69f6a2355b7f9d3e0c6d0bd7620
fabricioreisf <fabricioreisferreira@hotmail.com>
2026-04-06 10:49:57 -0300

    Initialize Repo

---
+++ contracts/Candidate.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.28;
+
+contract Candidate {
+    
+    address public owner;
+    uint256 public productCount;
+    uint256 public totalRevenue;
+    mapping(uint256 => Product) public products;
+    mapping(address => uint256) public purchases; 
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "Only owner can call this function");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    struct Product {
+        uint256 id;
+        string name;
+        uint256 price;
+        uint256 quantity;
+        bool exists;
+    }   
+
+    // Product management
+    function registrarProduto(string memory _nome, uint256 _preco, uint256 _quantidade) public onlyOwner returns (uint256) {}
+
+    function alterarPreco(uint256 _produtoId, uint256 _novoPreco) public onlyOwner {}
+
+    function obterProduto(uint256 _produtoId) public view returns (Product memory) {}
+
+    // Stock management
+    function verificarEstoque(uint256 _produtoId) public view returns (uint256) {}
+    
+    function adicionarEstoque(uint256 _produtoId, uint256 _quantidade) public onlyOwner {}
+
+    function removerDoEstoque(uint256 _produtoId, uint256 _quantidade) public onlyOwner {}
+
+    // Sales
+    function comprar(uint256 _produtoId, uint256 _quantidade) public payable {}
+
+    // Reports
+    function obterTotalComprado(address _usuario) public view returns (uint256) {}
+
+    function obterReceitaTotal() public view returns (uint256) {}
+
+    function obterSaldo() public view returns (uint256) {}
+
+    // Withdrawals
+    function sacar() public onlyOwner {}
+}
